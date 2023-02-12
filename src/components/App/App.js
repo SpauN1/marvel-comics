ddc6a7335c149a23ec233d7ce7ffa7218f2e40d6
Spaun1 <andreispaun7@gmail.com>
2023-02-12 13:27:19 +0300

    feat: dynamic display character when click on card

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppHeader from '../AppHeader/AppHeader';
 import CharInfo from '../CharInfo/CharInfo';
 import CharList from '../CharList/CharList';
@@ -5,20 +7,32 @@ import RandomChar from '../RandomChar/RandomChar';
 
 import decoration from '../../assets/img/vision.png';
 
-const App = () => {
-  return (
-    <div className="app">
-      <AppHeader />
-      <main>
-        <RandomChar />
-        <div className="char__content">
-          <CharList />
-          <CharInfo />
-        </div>
-        <img className="bg-decoration" src={decoration} alt="vision" />
-      </main>
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    selectedChar: null,
+  };
+
+  onCharSelected = (id) => {
+    this.setState({
+      selectedChar: id,
+    });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <AppHeader />
+        <main>
+          <RandomChar />
+          <div className="char__content">
+            <CharList onCharSelected={this.onCharSelected} />
+            <CharInfo charId={this.state.selectedChar} />
+          </div>
+          <img className="bg-decoration" src={decoration} alt="vision" />
+        </main>
+      </div>
+    );
+  }
+}
 
 export default App;
