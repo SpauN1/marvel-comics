5ac17d027f7fab45be98b105c39a0b4e84c8d5de
Spaun1 <andreispaun7@gmail.com>
2023-02-23 04:57:38 +0300

    feat: add color to active page

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,21 +1,30 @@
+import { NavLink, Link } from 'react-router-dom';
+
 import './AppHeader.scss';
+
+const isActive = ({ isActive }) => {
+  return { color: isActive ? '#9f0013' : 'inherit' };
+};
 
 const AppHeader = () => {
   return (
     <header className="app__header">
       <h1 className="app__title">
-        <a href="#">
+        <Link to="/">
           <span>Marvel</span> information portal
-        </a>
+        </Link>
       </h1>
       <nav className="app__menu">
         <ul>
           <li>
-            <a href="#">Characters</a>
+            <NavLink end to="/" style={isActive}>
+              Characters
+            </NavLink>
           </li>
-          /
           <li>
-            <a href="#">Comics</a>
+            <NavLink end to="/comics" style={isActive}>
+              Comics
+            </NavLink>
           </li>
         </ul>
       </nav>
