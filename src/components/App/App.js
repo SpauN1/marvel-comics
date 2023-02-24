946915839bf38dc8e697321f32b682c9f434a722
Spaun1 <andreispaun7@gmail.com>
2023-02-24 03:46:16 +0300

    feat: add single comics page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainPage from '../../pages/MainPage';
 import ComicsPage from '../../pages/ComicsPage';
 import Page404 from '../../pages/404';
+import SingleComicPage from '../../pages/SingleComicPage';
 import AppHeader from '../AppHeader/AppHeader';
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="/comics" element={<ComicsPage />} />
+            <Route path="/comics/:comicId" element={<SingleComicPage />} />
             <Route path="*" element={<Page404 />}></Route>
           </Routes>
         </main>
