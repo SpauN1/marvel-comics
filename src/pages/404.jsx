bc63f9a7d95a587682178fb1004f28aeb684333a
Spaun1 <andreispaun7@gmail.com>
2023-02-24 03:49:35 +0300

    feat: 404 page add button back to previous page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,14 +1,23 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import ErrorMessage from '../components/ErrorMessage/ErrorMessage';
 
 const Page404 = () => {
+  const navigate = useNavigate();
+  const goBack = () => navigate(-1);
   return (
     <div>
       <ErrorMessage />
       <p style={{ textAlign: 'center', fontWeight: 'bold', fontSize: '24px' }}>
         Page doesn't exist
       </p>
+      <button
+        className="button button__main button__long"
+        style={{ margin: '20px auto 0', display: 'block' }}
+        onClick={goBack}
+      >
+        <div className="inner">Go back</div>
+      </button>
       <Link
         style={{
           display: 'block',
