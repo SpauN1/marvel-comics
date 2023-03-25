16afae304c10a6672cac344afe8f351c0d0d98e5
Spaun1 <andreispaun7@gmail.com>
2023-03-25 12:05:17 +0300

    feat: add meta tags with description for comics

--- src/pages/ComicsPage.jsx
+++ src/pages/ComicsPage.jsx
@@ -1,9 +1,15 @@
+import { Helmet } from 'react-helmet';
+
 import ComicsList from '../components/ComicsList/ComicsList';
 import AppBanner from '../components/AppBanner/AppBanner';
 
 const ComicsPage = () => {
   return (
     <>
+      <Helmet>
+        <meta name="description" content="Page with list of our comics" />
+        <title>Comics page</title>
+      </Helmet>
       <AppBanner />
       <ComicsList />
     </>
